8ebcedbb37e1bf3d5fa508bafed8ace3347f6e08
Eugene Golovanov <eugene.golovanov@hart.com>
2016-07-20 13:40:13 -0700

    socket added

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ var PORT = process.env.PORT || 3000; // process.env.PORT - port from heroku, if 
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
+
+io.on('connection', function () {
+	console.log('User connected via socket.io');
+});
+
+
 http.listen(PORT, function () {
-	console.log('Server started!');
+	console.log('Server started! port:' + PORT);
 });
