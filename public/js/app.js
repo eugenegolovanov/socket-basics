929d735a8e716556168ec02055afe21a7d3b1006
Eugene Golovanov <eugene.golovanov@hart.com>
2016-07-20 16:20:13 -0700

    Front end form to submit added

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,40 @@
 	var socket = io();//io() - function defined in socket.io-x.x.x.js file
  	// socket.connect('http://localhost:3011');
 
+
+ 	//listen to the 'connect' event. when we connected to server
  	socket.on('connect', function () {
-	 	console.log('Message from front end');
+	 	console.log('We connected with socket');
  	});
 
+ 	//listen to the custom 'message' event that we created at server.js
+ 	 socket.on('message', function (message) {
+	 	console.log('Message text: ' + message.text);
+ 	});
+
+
+
+
+//Handle submitting of new message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function (event) {
+	event.preventDefault();//prevent refreshing page when button pressed
+
+	$messageField = $form.find('input[name=message]');
+
+	//Sending message with text extracted from form
+	socket.emit('message' , {
+		text: $messageField.val() //.val() pull value out and return it as string
+	});
+
+	//Make field empty
+	$messageField.val("");
+
+	// var text = $('#textField').val();
+	// console.log('----------------------------------------------------');
+	// console.log('Text: ' + text);
+	// console.log('----------------------------------------------------');
+	// $('#textField').val("");
+
+});
--- server.js
+++ server.js
@@ -7,8 +7,25 @@ var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/public'));
 
 
-io.on('connection', function () {
+io.on('connection', function (socket) {
 	console.log('User connected via socket.io');
+
+
+	socket.on('message', function (message) {
+		console.log('Message received: ' + message.text);
+
+		// io.emit;//sending to everyone
+		socket.broadcast.emit('message', message);//sending to everyone except sender
+	});
+
+
+
+
+
+	//socket emit - emits the custom event, 'message' - custom name of the event
+	socket.emit('message', {
+		text: 'Welcome to chat app'
+	});
 });
 
 
